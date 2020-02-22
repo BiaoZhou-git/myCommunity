72b42ecbdfbf4399f9b9ccac868d3b3ac05ee0ec
ZBKeepSilence <ZB_silence@163.com>
2020-02-22 21:47:08 +0800

    init repo

---
+++ src/test/java/com/shangma/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shangma.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
